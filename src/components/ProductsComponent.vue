477efd0cd49c7cb1856ea1eb539b11d60aeb83c5
PedIury <iurypedro07@gmail.com>
2023-12-02 01:29:11 -0300

    Initial commit

---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <!-- PRODUTOS -->
+  <div class="produtos pt-32 pb-64">
+    <div class="container">
+      <h3>Produtos</h3>
+      <div class="lista-produtos display-flex align-items-center flex-wrap-wrap gap-16 pt-16">
+        <div 
+          class="produto"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <img 
+            :src="product.images" 
+            alt="Categoria X"
+            width="232"
+            height="258"
+          >
+          <div class="detalhes">
+            <p class="nome-produto">{{product.title}}</p>
+            <p class="categoria">{{product.category.name}}</p>
+            <p class="preco">R$ {{product.price}}</p>
+            <button
+              class="carrinho"
+              :class="{ 'no-carrinho': isProductInCart(product.id) }"
+              @click="toggleCart(product)"
+            >
+              {{ isProductInCart(product.id) ? 'Adicionado' : 'Adicionar ao Carrinho' }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  name: 'ProductsComponent',
+  computed: {
+    ...mapState(['cart']),
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    this.requestProductsAPI();
+  },
+  methods: {
+    ...mapMutations(['addToCart', 'removeFromCart']),
+    async requestProductsAPI() {
+      try {
+        const response = await fetch(`https://api.escuelajs.co/api/v1/products`);
+        const data = await response.json();
+        this.products = data.filter(product => [15, 16, 17, 19, 23, 24, 25, 26].includes(product.id));
+      } catch (error) {
+        console.error('Ocorreu um erro: ', error);
+      }
+    },
+    isProductInCart(productId) {
+      return this.cart.some(item => item === productId);
+    },
+    toggleCart(product) {
+      const productId = product.id;
+      if (this.isProductInCart(productId)) {
+        this.removeFromCart(productId);
+      } else {
+        this.addToCart(productId);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.lista-produtos {
+  justify-content: space-between;
+}
+
+img {
+  height: revert-layer;
+}
+</style>
